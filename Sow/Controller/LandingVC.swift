7eac058390cab020ce77ad342e297f1d7ef6af95
matt <mattmega4@gmail.com>
2019-04-17 02:11:18 -0700

    added landingvc

---
+++ Sow/Controller/LandingVC.swift
@@ -0,0 +1,21 @@
+//
+//  LandingVC.swift
+//  Sow
+//
+//  Created by Matthew Singleton on 4/17/19.
+//  Copyright © 2019 Matthew Howes Singleton. All rights reserved.
+//
+
+import UIKit
+
+class LandingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
